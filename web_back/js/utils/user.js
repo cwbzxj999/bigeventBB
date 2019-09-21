d7c6ab1ea61cc64bdfcb1a5a71ea5f624ba248b0
cwbzxj999 <cwb19971015@163.com>
2019-09-21 17:48:18 +0800

    登出功能和获取用户信息功能完成啦

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,11 +1,33 @@
 var user = {
-  login: function(userName,password,callBack){
-    $.post('http://localhost:8000/admin/login',{user_name:userName,password:password},function(res){
+  /**
+   * 
+   * @param {*} userName 
+   * @param {*} password 
+   * @param {*} callBack 
+   */
+  login: function (userName, password, callBack) {
+    $.post(URLIST.user_login, {
+      user_name: userName,
+      password: password
+    }, function (res) {
       // res是本次请求，从服务器回来的数据
       console.log(res);
       callBack(res);
     });
   },
 
-  
-}
+
+  logout: function (callBack) {
+    $.post(URLIST.user_logout, function (res) {
+      callBack(res);
+    });
+  },
+
+
+  getInfo: function (callBack) {
+    $.get(URLIST.user_info, function (res) {
+      callBack(res);
+    });
+  }
+
+};
